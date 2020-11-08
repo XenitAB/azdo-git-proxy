3eddf1dc02bc7668fbebe830701ba29279595d9e
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-11-08 17:27:44 +0100

    Pull branch if it already exists

--- cmd/git-proxy/main.go
+++ cmd/git-proxy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"strings"
@@ -54,21 +55,105 @@ func ProxyMiddleware(next http.Handler, repoPath string) http.Handler {
 		log.Printf("Repository: %s", azdoRepo)
 		log.Printf("repoUri: %s", repoUri)
 		cloneOptions := &git.CloneOptions{
-			Bare:           true,
+			Bare:           false,
 			CheckoutBranch: "master",
 		}
-		repo, err := git.Clone(repoUri, localPath, cloneOptions)
+		_, err := git.Clone(repoUri, localPath, cloneOptions)
 		if err != nil {
-			log.Printf("LOCAL REPO ALREADY EXISTS - FETCH INSTEAD: %s", err)
-			remote, err := repo.Remotes.Lookup("origin")
+			err := PullBranch(localPath, "origin", "master", "", "", "TEST-NAME", "test-email@example.com")
 			if err != nil {
-				log.Printf("Unable to locate origin: %s", err)
-			}
-			if err := remote.Fetch([]string{}, nil, ""); err != nil {
-				log.Printf("Unable to fetch remote: %s", err)
+				log.Printf("Error pulling branch: %s", err)
 			}
 
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func PullBranch(repoPath string, remoteName string, branchName string, user string, pass string, name string, email string) error {
+
+	repo, err := git.OpenRepository(repoPath)
+	if err != nil {
+		return err
+	}
+
+	remote, err := repo.Remotes.Lookup(remoteName)
+	if err != nil {
+		return err
+	}
+
+	err = remote.Fetch([]string{}, &git.FetchOptions{}, "")
+
+	if err != nil {
+		return err
+	}
+
+	remoteBranch, err := repo.References.Lookup("refs/remotes/" + remoteName + "/" + branchName)
+	if err != nil {
+		return err
+	}
+
+	mergeRemoteHead, err := repo.AnnotatedCommitFromRef(remoteBranch)
+	if err != nil {
+		return err
+	}
+
+	mergeHeads := make([]*git.AnnotatedCommit, 1)
+	mergeHeads[0] = mergeRemoteHead
+	if err = repo.Merge(mergeHeads, nil, nil); err != nil {
+		return err
+	}
+
+	// Check if the index has conflicts after the merge
+	idx, err := repo.Index()
+	if err != nil {
+		return err
+	}
+
+	currentBranch, err := repo.Head()
+	if err != nil {
+		return err
+	}
+
+	localCommit, err := repo.LookupCommit(currentBranch.Target())
+	if err != nil {
+		return err
+	}
+
+	// If index has conflicts, read old tree into index and
+	// return an error.
+	if idx.HasConflicts() {
+
+		repo.ResetToCommit(localCommit, git.ResetHard, &git.CheckoutOpts{})
+
+		repo.StateCleanup()
+
+		return errors.New("conflict")
+	}
+
+	// If everything looks fine, create a commit with the two parents
+	treeID, err := idx.WriteTree()
+	if err != nil {
+		return err
+	}
+
+	tree, err := repo.LookupTree(treeID)
+	if err != nil {
+		return err
+	}
+
+	remoteCommit, err := repo.LookupCommit(remoteBranch.Target())
+	if err != nil {
+		return err
+	}
+
+	sig := &git.Signature{Name: name, Email: email, When: time.Now()}
+	_, err = repo.CreateCommit("HEAD", sig, sig, "merged", tree, localCommit, remoteCommit)
+	if err != nil {
+		return err
+	}
+
+	repo.StateCleanup()
+
+	return nil
 }
